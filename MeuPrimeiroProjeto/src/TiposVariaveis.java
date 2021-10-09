09ebbd8416f99af669bbd9d97ffe057447c6bf0b
jadergreiner <jadergreiner@gmail.com>
2021-10-09 13:56:31 -0300

    Primeiro programa em Java

---
+++ MeuPrimeiroProjeto/src/TiposVariaveis.java
@@ -0,0 +1,33 @@
+
+public class TiposVariaveis {
+	
+	public static void main(String[] args) {
+		
+		byte tipoByte = 123;
+		
+		short tipoShort = 12345;
+		
+		char tipoChar = 'A';
+		
+		float tipoFloat = 10.15f;
+		
+		double tipoDouble = 1.52;
+		
+		int tipoInt = 7654321;
+		
+		long tipoLong = 1987654321L;
+		
+		boolean tipoBooleano = true;
+		
+		System.out.println("Valor do tipoByte = " + tipoByte);
+		System.out.println("Valor do tipoShort = " + tipoShort);
+		System.out.println("Valor do tipoChar = "  + tipoChar);
+		System.out.println("Valor do tipoFloat = " +tipoFloat);
+		System.out.println("Valor do tipoDouble = " +tipoDouble);
+		System.out.println("Valor do tipoInt = " +tipoInt);
+		System.out.println("Valor do tipoLong = " +tipoLong);
+		System.out.println("Valor do tipoBooleano = " +tipoBooleano);
+		
+	}
+
+}
